f3e0dcc2cca38208ec91a34339e2c556120fcfd0
Andre Aquino <aquino_andre.agustin@yahoo.com>
2022-10-20 19:58:57 +0800

    added base preliminaries

---
+++ SA2/src/MyApp/GamePanel.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyApp;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author killa
+ */
+public class GamePanel extends javax.swing.JPanel implements ActionListener {
+    // panel properties
+    private int WIDTH = 700, HEIGHT = 700;
+    
+    // handles the redrawing of the game
+    private Timer timer;
+    
+    public GamePanel() {
+        // properties
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        
+        // redraws the game every 8 milliseconds
+        timer = new Timer(8, this);
+        timer.start();
+    }
+    
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(WIDTH, HEIGHT);
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        // draw background
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        
+        g.dispose();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }    
+}
